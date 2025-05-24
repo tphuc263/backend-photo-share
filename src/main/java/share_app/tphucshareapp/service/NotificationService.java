1eb5374b96ab67cdeb3c9fa3da3387499d6e4bf3
tphuc263 <tphuc.ptit@gmail.com>
2025-05-24 14:26:58 +0700

    update

---
+++ src/main/java/share_app/tphucshareapp/service/NotificationService.java
@@ -0,0 +1,4 @@
+package share_app.tphucshareapp.service;
+
+public class NotificationService {
+}
--- src/main/java/share_app/tphucshareapp/service/photo/INewsfeedService.java
+++ src/main/java/share_app/tphucshareapp/service/photo/INewsfeedService.java
@@ -4,9 +4,4 @@ import org.springframework.data.domain.Page;
 import share_app.tphucshareapp.dto.response.photo.PhotoResponse;
 
 public interface INewsfeedService {
-    Page<PhotoResponse> getNewsfeed(int page, int size);
-
-    Page<PhotoResponse> getRecommendations(int page, int size);
-
-    Page<PhotoResponse> getTrendingPhotos(int page, int size);
 }
